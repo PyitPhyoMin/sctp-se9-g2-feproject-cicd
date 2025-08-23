df822c2656af380d8f821fad40447773296ad603
PyitPhyoMin <pyitphyo.min@gmail.com>
2025-08-23 21:30:00 +0800

    removing the HomePage test line

--- src/pages/HomePage/HomePageView.js
+++ src/pages/HomePage/HomePageView.js
@@ -17,7 +17,6 @@ function HomePageView({ selectedProducts }) {
       {selectedProducts !== undefined && selectedProducts.length > 0 && (
         <section className={styles.sectionStyle}>
           <section>
-            <div>This is the release branch update.</div>
             <div>
               <div className={styles.headerStyle}>
                 <section className={styles.sectionStyle}>
